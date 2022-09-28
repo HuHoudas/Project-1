f2d201c09ba8a31211a8f1ffe01e8d19f2d1980c
HuHoudas <hugo.houdas@epitech.eu>
2022-09-28 14:44:11 +0200

    start Soft_Skills page

---
+++ Soft_Skills.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/style.css">
+    <title>Soft_Skills</title>
+</head>
+
+<body>
+
+<!-- require header.php -->
+    <div>
+        <h2>Soft Skills</h2>
+        <ul>
+            <li>Teamwork</li>
+            <li>Communication</li>
+            <li>Time management</li>
+            <li>Problem solving</li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </div>
+    <div>
+        <h2>Competence</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Outils</th>
+                    <th>Language</th>
+                    <th>OS</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>VSCode</td>
+                    <td>HTML</td>
+                    <td>Windows</td>
+                </tr>
+                <tr>
+                    <td>Notepade++</td>
+                    <td>CSS</td>
+                    <td>IOS</td>
+                </tr>
+                <tr>
+                    <td>PowerPoint</td>
+                    <td>JS</td>
+                    <td>Android</td>
+                </tr>
+                <tr>
+                    <td>WordPress</td>
+                    <td>PHP</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!-- require footer.php -->
+</body>
+
+</html>
