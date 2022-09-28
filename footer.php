477e2996228bf70259aad3a06d476ae35b79c828
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-09-28 16:26:19 +0200

    creation footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="footer.css">
+
+</head>
+<body>
+    
+</body>
+
+<footer class="contacts">
+    <ul>
+        <li>schtroumpf.patissier@gmail.com</li>
+        <li>Tel: 06 55 51 11</li>
+        <li>1000 Vallée de Schtroumpf</li>
+    </ul>
+</footer>
+
+</html>
---
+++ loisir.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+
+<div class="loisirs">
+    <figure>
+        <figcaption>
+            <img src="./asset/plage.jpg" alt="Natation">
+            <h2>La Natation</h2>
+            <p>This is my text.</p>
+        </figcaption>
+    </figure>
+    <figure>
+        <figcaption>
+            <img src="./asset/poesie.jpg" alt="Poesie">
+            <h2>La poésie</h2>
+            <p>This is my text.</p>
+        </figcaption>
+    </figure>
+    <figure>
+        <figcaption>
+            <img src="./asset/musique.png" alt="Musique">
+            <h2>La Musique</h2>
+            <p>This is my text.</p>
+        </figcaption>
+    </figure>
+    <figure>
+        <figcaption>
+            <img src="./asset/cooking.jpeg" alt="Cusine">
+            <h2>La Cusine</h2>
+            <p>This is my text.</p>
+        </figcaption>
+    </figure>
+    <figure>
+        <figcaption>
+            <img src="./asset/aviation.jpg" alt="Aviation">
+            <h2>L'Aviation</h2>
+            <p>This is my text.</p>
+        </figcaption>
+    </figure>
+</div>
+</body>
+<?php
+include "footer.php"
+?>
+
+</html>
