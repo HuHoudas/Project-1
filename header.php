9459bf927cec37a9ced4a34c1a64b507655b672b
HuHoudas <hugo.houdas@epitech.eu>
2022-09-29 14:53:34 +0200

    link all css and php page

---
+++ header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/style.css">
+    <title>Projet CV</title>
+
+</head>
+
+    <header>
+
+        <img class="image"
+            src="https://sites.google.com/site/villageschtroumpfs/_/rsrc/1441410130483/patisier.gif?height=168&width=300"
+            alt="image du strounf patitier">
+        <div>
+            <div>
+                <h1> schtroumpf patissier</h1>
+            </div>
+            <div>
+                <h2> reconvertion développeur web</h2>
+            </div>
+        </div>
+
+
+    </header>
+    <nav>
+        <div class="nav-list">
+            <a class="nav-item" href="index.php">profil</a>
+            <a class="nav-item" href="form.php">Expe/Form</a>
+            <a class="nav-item" href="Soft_Skills.php">comp</a>
+            <a class="nav-item" href="loisir.php">Loisir</a>
+        </div>
+    </nav>
+
+<body>
+</body>
+
+</html>
