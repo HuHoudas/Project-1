895841095b945d5c98fbd93ac9ab34f53466b836
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-10-02 11:52:52 +0200

    commit uptodate

--- header.php
+++ header.php
@@ -9,19 +9,11 @@
   <title>Projet CV</title>
 
 </head>
-<<<<<<< HEAD
-    <header>
-
-        <img class="image"
-            src="https://sites.google.com/site/villageschtroumpfs/_/rsrc/1441410130483/patisier.gif?height=168&width=300"
-            alt="image du strounf patitier">
-=======
 
 <header>
   <!--         <div>
             <h2> schtroumpf patissier</h1>
         </div>
->>>>>>> fcd4364ba4e2cd59f658dc59ad117e4cec405bcc
         <div>
             <h2> reconvertion développeur web</h2>
         </div>
@@ -32,24 +24,12 @@
     <ul>
       <li><a href="index.php">Home</a></li>
       <li><a href="form.php">Expérience</a></li>
-      <li><a href="loisir.php">Loisir</a></li>
+      <li><a href="loisirs.php">Loisir</a></li>
       <li><a href="profil.php">profil</a></li>
 
-<<<<<<< HEAD
-    </header>
-    <nav>
-        <div class="nav-list">
-            <a class="nav-item" href="index.php">Profil</a>
-            <a class="nav-item" href="form.php">Expériences et formation</a>
-            <a class="nav-item" href="Soft_Skills.php">Compétences générales</a>
-            <a class="nav-item" href="loisirs.php">Loisirs</a>
-        </div>
-    </nav>
-=======
     </ul>
     <img src="asset/schtroumpfs-image-animee-0005.gif" alt="mushroom house">
   </div>
->>>>>>> fcd4364ba4e2cd59f658dc59ad117e4cec405bcc
 
   <a href="#" id="openBtn">
     <span class="burger-icon">
