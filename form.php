a0d5941668a222eba3a313faf1e542e0e324efdc
GARCIA_STEPHANIE <45000steph@gmail.com>
2022-10-05 16:37:16 +0200

    d

--- form.php
+++ form.php
@@ -14,48 +14,39 @@
 <?php include "header.php" ?>
 <body>
     <div>
-    <h1 class="nav-pro">Expereience profetionnelle</h1>
         <ul class="nav-pro">
             <div>
+                <h1 class="nav-pro">Expereience profetionnelle</h1>
                 <div>
                     <p>2002-2003</p>
-                    <li ><a href=#></a> Chef patissier dans la vallé des schtroumpf</a></li>
+                    <li >Chef patissier dans la vallé des schtroumpf</a></li>
 
                 </div>
                 <div>
                     <p>2001-2002</p>
-                </div>
-                <div>
                     <li><a href=#></a>Commit de cuisine au grand schtroumpf</li>
                 </div>
+                
                 <div>
-                    <p>2001-2000</p>
+                    <p>2000-2001</p>
                     <li ><a href=#></a>Apprenti schtroumpf patitier</li>
                 </div>
                 <div>
                     <p>1999-2000</p>
                     <li><a href=#></a>Preparateur de schtroumpfcommande</li>
                 </div>
-                <h2>formation/Diplôme</h2>
             </div>
         </ul>
     </div>
-    <ul class="nav-pro">
-        <div>
-            <div>
-                <li class="nav-esp"><a href=#></a> obtention du schroumpf patitier</a></li>
-            </div>
-            <div>
-                <li class="nav-esp"><a href=#></a></li>
-            </div>
-            <div>
-                <li class="nav-esp"><a href=#></a></li>
-            </div>
-            <div>
-                <li class="nav-esp"><a href=#></a></li>
-            </div>
+    <div>
+        <div class="nav-pro">
+            <h2 class="nav-pro">formation/Diplôme</h2>
+            
+                <li > obtention du schroumpf patitier</a></li>
+           
+            
         </div>
-    </ul>
+    
     </div>
 </body>
 <?php include "footer.php" ?>
