a159cf7f4287a75c271d4e246f374a357d7d23b1
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-10-05 11:34:00 +0200

    Contact Form

---
+++ messagemoi.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Message Moi</title>
+    <link rel="stylesheet" href="./asset/style.css">
+    <link rel="stylesheet" href="./asset/header.css">
+    <link rel="stylesheet" href="./asset/footer.css">
+    <link rel="stylesheet" href="./asset/messagemoi.css">
+
+</head>
+<header>
+  <div id="mySidenav" class="sidenav">
+    <a id="closeBtn" href="#" class="close">×</a>
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="form.php">Expérience</a></li>
+      <li><a href="loisirs.php">Loisir</a></li>
+      
+
+    </ul>
+    <img src="asset/schtroumpfs-image-animee-0005.gif" alt="mushroom house">
+  </div>
+
+  <a href="#" id="openBtn">
+    <span class="burger-icon">
+      <span></span>
+      <span></span>
+      <span></span>
+    </span>
+  </a>
+  <script src="asset/index.js"></script>
+</header>
+
+<body>
+<section id="contact">
+  <h1>
+    <element id="bluetext">
+    Contacter le Schtroumpf !
+    </element>
+  </h1>
+  <form id="contactform">
+    <label for="firstname">Prénom</label>
+    <input type="text" id="firstname" name="firstname">
+
+    <label for="email">Email</label>
+    <input type="email" id="email" name="email">
+
+    <label for="message">Message</label>
+    <textarea name="message" id="" cols="30" rows="10"></textarea>
+    <button>Envoyer</button>
+</body>
+
+
+
+</html>
