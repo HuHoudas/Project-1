785645304ff3fb127b046fb3ebda9186dca91058
Violeta Doncheva <violeta.doncheva90@gmail.com>
2022-10-05 14:48:47 +0200

    new update

--- footer.php
+++ footer.php
@@ -15,7 +15,9 @@
 
 <footer class="contacts">
     <ul>
+        <li>schtroumpf.patisserie@gmail.com</li>
         <li><button><a href="messagemoi.php">SCHTROUMPF MOI !</a></button></li>
+        <li>00 01 02 03 04 05 06 07 08 09</li>
     </ul>
 </footer>
 </html>
